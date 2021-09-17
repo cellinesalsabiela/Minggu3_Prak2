93f05d8504b542180caac10be162a91abdef79b4
cellinesalsabiela <cellinesalsabiela574@gmail.com>
2021-09-17 10:12:15 +0700

    pertemuan3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class,'home']);
+Route::get('/profil', [ProfilController::class,'profil']);
